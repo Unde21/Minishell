0d45d29c18f74dbe0353183424f30a7c0449d103
Sammy Maouche <samaouch@z2r11p2.42lyon.fr>
2025-06-13 13:40:37 +0200

    refacto(get_input): norm

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,3 +6,12 @@ bool	print_err(char *str_error)
 	ft_dprintf(2, "%s", str_error);
 	return (false);
 }
+
+void	reset_g_return_value(t_data *data)
+{
+	if (g_return_value != 0)
+	{
+		data->return_value = g_return_value;
+		g_return_value = 0;
+	}
+}
