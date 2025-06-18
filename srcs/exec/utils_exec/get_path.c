0d3d71f5d54a833309831fa6edf729674a0eeacd
samaouch <maouche.sammy@gmail.com>
2025-06-18 16:24:46 +0200

    fix merge

--- srcs/exec/utils_exec/get_path.c
+++ srcs/exec/utils_exec/get_path.c
@@ -2,7 +2,7 @@
 #include "parsing.h"
 #include <stdlib.h>
 
-static char	*search_path_in_env(t_data *data, int *return_value)
+static char	*search_path_in_env(t_data *data)
 {
 	char	*path_value;
 	size_t	i;
@@ -16,8 +16,8 @@ static char	*search_path_in_env(t_data *data, int *return_value)
 			path_value = get_value(data, data->env_array[i]);
 			if (!path_value || path_value[0] == '\0')
 			{
-				if (*return_value == 0)
-					*return_value = 127;
+				if (data->return_value == 0)
+					data->return_value = 127;
 				return (NULL);
 			}
 			break ;
@@ -25,11 +25,11 @@ static char	*search_path_in_env(t_data *data, int *return_value)
 		++i;
 	}
 	if (path_value == NULL)
-		*return_value = 127;
+		data->return_value = 127;
 	return (path_value);
 }
 
-char	*get_strict_path(char **path, char **params, int *return_value)
+char	*get_strict_path(char **path, char **params, t_data *data)
 {
 	char	*path_value;
 	size_t	i;
@@ -42,11 +42,11 @@ char	*get_strict_path(char **path, char **params, int *return_value)
 		path_value = ft_strjoin_and_free(path_value, params[0]);
 		if (!path_value)
 		{
-			*return_value = 1;
+			data->return_value = 1;
 			free_all(path);
 			return (NULL);
 		}
-		if (is_access_ok(path_value, return_value, path))
+		if (is_access_ok(path_value, path, data))
 		{
 			free_all(path);
 			return (path_value);
@@ -58,16 +58,16 @@ char	*get_strict_path(char **path, char **params, int *return_value)
 	return (NULL);
 }
 
-char	*get_path_cmd(t_data *data, char **params, int *return_value)
+char	*get_path_cmd(t_data *data, char **params)
 {
 	char	*path_value;
 	char	**path;
 
-	path_value = search_path_in_env(data, return_value);
+	path_value = search_path_in_env(data);
 	if (path_value == NULL)
 	{
-		if (*return_value == 0)
-			*return_value = 1;
+		if (data->return_value == 0)
+			data->return_value = 1;
 		return (NULL);
 	}
 	path = ft_split(path_value, ':');
@@ -75,8 +75,8 @@ char	*get_path_cmd(t_data *data, char **params, int *return_value)
 	path_value = NULL;
 	if (!path)
 	{
-		*return_value = 1;
+		data->return_value = 1;
 		return (NULL);
 	}
-	return (get_strict_path(path, params, return_value));
+	return (get_strict_path(path, params, data));
 }
