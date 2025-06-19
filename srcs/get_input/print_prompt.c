98b8bdd9beeb382e45fe6ed0eb1011a0e558ac02
samaouch <maouche.sammy@gmail.com>
2025-06-19 16:39:40 +0200

    fixbug(prompt): fix display bug when en -i ./minishell

--- srcs/get_input/print_prompt.c
+++ srcs/get_input/print_prompt.c
@@ -2,11 +2,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+static bool	prompt_without_env(t_data *data, char **prompt)
+{
+	if (data->env == NULL || data->env[0] == NULL)
+	{
+		*prompt = ft_strdup("Minishell--> ");
+		if (*prompt == NULL)
+		{
+			data->return_value = 1;
+			return (print_err_false(ERR_MALLOC));
+		}
+		return (false);
+	}
+	return (true);
+}
+
+static void	create_prompt(t_data *data, char **prompt, char *code, char *path)
+{
+	if (data->return_value != 0)
+		*prompt = ft_strdup(RED CROSS);
+	else
+		*prompt = ft_strdup(GREEN CHECK);
+	if (*prompt == NULL)
+	{
+		data->return_value = 1;
+		print_err_false(ERR_MALLOC);
+		return ;
+	}
+	*prompt = ft_strjoin_and_free(*prompt, code);
+	*prompt = ft_strjoin_and_free(*prompt, END_BLUE);
+	*prompt = ft_strjoin_and_free(*prompt, path);
+	*prompt = ft_strjoin_and_free(*prompt, END_COLOR);
+	if (*prompt == NULL)
+	{
+		data->return_value = 1;
+		print_err_false(ERR_MALLOC);
+		return ;
+	}
+}
+
 void	get_prompt(t_data *data, char **prompt)
 {
 	char	*code;
 	char	*path;
 
+	if (prompt_without_env(data, prompt) == false)
+		return ;
 	path = getcwd(NULL, 0);
 	if (path == NULL)
 	{
@@ -16,15 +57,13 @@ void	get_prompt(t_data *data, char **prompt)
 	code = ft_itoa(data->return_value);
 	if (code != NULL)
 	{
-		if (data->return_value != 0)
-			*prompt = ft_strdup(RED CROSS);
-		else
-			*prompt = ft_strdup(GREEN CHECK);
-		*prompt = ft_strjoin_and_free(*prompt, code);
-		*prompt = ft_strjoin_and_free(*prompt, END_BLUE);
-		*prompt = ft_strjoin_and_free(*prompt, path);
-		*prompt = ft_strjoin_and_free(*prompt, END_COLOR);
+		create_prompt(data, prompt, code, path);
 		free(code);
+	}
+	else
+	{
+		data->return_value = 1;
+		print_err_false(ERR_MALLOC);
 	}
 	free(path);
 }
--- srcs/parsing/utils_parsing.c
+++ srcs/parsing/utils_parsing.c
@@ -71,6 +71,6 @@ char	*ft_strjoin_and_free(char *s1, char *s2)
 	while (s2[++j])
 		join[i + j] = s2[j];
 	join[i + j] = '\0';
-	free(s1);1
+	free(s1);
 	return (join);
 }
